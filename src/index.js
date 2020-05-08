1385d51d78fc2d66ae9721eea4d98f64a791f781
姜磊 <1021124094@qq.com>
2020-05-08 10:23:48 +0800

    ADD: State Hook

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import ComponentLife from './ComponentLife'
 import SetStateDemo from './setStateDemo'
 import IfDemo from './ifDemo'
 import KeyDemo from './keyDemo'
+import HookDemo from './HookDemo'
 // function tick(params) {
 //   const element = (
 //     <div>
@@ -16,7 +17,7 @@ import KeyDemo from './keyDemo'
 //   ReactDOM.render(element, document.getElementById('root'))
 // }
 // setInterval(tick, 1000)
-ReactDOM.render(<KeyDemo></KeyDemo>, document.getElementById('root'))
+ReactDOM.render(<HookDemo></HookDemo>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
