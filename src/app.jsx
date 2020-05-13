01269b5dc21b19965ad899ea9e0313c7eb70482d
姜磊 <1021124094@qq.com>
2020-05-13 21:38:52 +0800

    ADD: react-router-dom

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
+import Home from './page/Home'
 // 用类的形式创建组件，Hook形式
 class App extends React.Component {
   //渲染函数
   render() {
-    return <h1>{this.props.a}</h1>
+    return <Route path="/home" component={Home}></Route>
   }
 }
 export default App
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import HookDemo from './HookDemo'
 import FormDemo from './FormDemo'
 import FetchDemo from './FetchDemo'
 import ProxyDemo from './ProxyDemo'
+import App from './app'
 // function tick(params) {
 //   const element = (
 //     <div>
@@ -20,7 +21,7 @@ import ProxyDemo from './ProxyDemo'
 //   ReactDOM.render(element, document.getElementById('root'))
 // }
 // setInterval(tick, 1000)
-ReactDOM.render(<ProxyDemo></ProxyDemo>, document.getElementById('root'))
+ReactDOM.render(<App></App>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
