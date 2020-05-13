5990683400247af813f3e5b3423016b0f666acaf
姜磊 <1021124094@qq.com>
2020-05-13 23:31:20 +0800

    ADD: react-router-dom 入门

---
+++ react-router-demo/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './pages/Home'
+import Mine from './pages/Mine'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/home" component={Home}></Route>
+        <Route path="/mine" component={Mine}></Route>
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ react-router-demo/src/pages/Mine.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Mine = () => {
+  return (
+    <div>
+      Mine
+    </div>
+  )
+}
+export default Mine
