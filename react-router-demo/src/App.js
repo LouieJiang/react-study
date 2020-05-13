468f1a09da52d360ee15044739c057a49b3afb5c
姜磊 <1021124094@qq.com>
2020-05-13 23:59:02 +0800

    ADD: <Link> exact

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import Home from './pages/Home'
 import Mine from './pages/Mine'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Ucenter from './pages/Ucenter'
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/home" component={Home}></Route>
-        <Route path="/mine" component={Mine}></Route>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/mine">Mine</Link>
+          </li>
+          <li>
+            <Link to="/mine/ucenter">Ucenter</Link>
+          </li>
+        </ul>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/mine" component={Mine}></Route>
+        <Route path="/mine/ucenter" component={Ucenter}></Route>
       </Router>
     </div>
   )
